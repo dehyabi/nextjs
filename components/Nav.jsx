5ec726f15fb0a2fd4e0ab58817da70f1af962a18
dehyabi <dehyasvibe@gmail.com>
2024-03-29 05:30:40 +0700

    fix(google login): show photo profile on desktop

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -48,7 +48,7 @@ const Nav = () => {
 
             <Link href="/profile">
               <Image
-                src="/assets/images/profile.png"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
